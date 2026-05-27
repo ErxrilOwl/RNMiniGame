01e27a0f610cf2f3b3d2abac01b65ef966637658
Nikko <atuan.nikko@gmail.com>
2026-05-27 22:07:50 +0800

    feat: added summary screen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -30,6 +30,11 @@ export const GameScreen = ({ userNumber, onGameOver }) => {
         }
     }, [currentGuess, userNumber, onGameOver])
 
+    useEffect(() => {
+        minBoundary = 1;
+        maxBoundary = 100;
+    })
+
     const nextGuessHandler = (direction) => {
         if (
             (direction === 'lower' && currentGuess < userNumber) || 
