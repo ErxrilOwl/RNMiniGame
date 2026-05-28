293eca4646859c7277bf3cfe8e45ec010d465176
Nikko <atuan.nikko@gmail.com>
2026-05-28 21:24:54 +0800

    feat: added dynamic width

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -92,7 +92,8 @@ export const GameScreen = ({ userNumber, onGameOver }) => {
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        padding: 24
+        padding: 24,
+        alignItems: 'center'
     },
     instructionText: {
         marginBottom: 12
